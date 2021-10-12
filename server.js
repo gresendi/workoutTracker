3fe49aa8bea6c98d52928d0753ac924bbbd3d402
Gilberto Resendiz <gresendi@uci.edu>
2021-10-11 22:27:14 -0700

    testing new heroku deployment

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
 
 app.use(require('./routes/api.js'))
 app.use(require('./routes/views.js'))
-
+//run
 app.listen(3000, () => {
   console.log(`running`)
 })
